7b6e1155897d230a89eeb405925718f8ff568b68
mikwl <jauntymike@gmail.com>
2021-05-31 00:17:45 +0300

    Added EmployeeSerializer

--- employees/models.py
+++ employees/models.py
@@ -7,7 +7,7 @@ class Employee(models.Model):
     last_name = models.CharField("Фамилия", max_length=30)
     first_name = models.CharField("Имя", max_length=30)
     patronymic = models.CharField("Отчество", max_length=30)
-    position = models.CharField("Должность", max_length=100)
+    position = models.CharField(max_length=100)
 
     JR = "Jn"
     MD = "Md"
@@ -22,8 +22,9 @@ class Employee(models.Model):
         (CE, "CEO"),
     ]
 
-    level = models.CharField("Уровень", max_length=100, choices=POSITION_CHOICES, blank=False, default=JR)
+    level = models.CharField(max_length=100, choices=POSITION_CHOICES, blank=False, default=JR)
     employment_date = models.DateField("Дата приёма на работу", default=localdate)
+    email = models.EmailField('Email', max_length=200, unique=True, default="email@example.com")
     salary = models.DecimalField(
         "Зарплата", max_digits=8, decimal_places=2, default=0, help_text="указывать сумму в долларах"
     )
